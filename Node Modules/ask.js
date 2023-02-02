8c2d7f746e4f098fb4109cc4e3301d76dbc4f508
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-02 22:19:21 +0530

    practise

---
+++ Node Modules/ask.js
@@ -0,0 +1,9 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.question("How do you like node?", (ans) => {
+	console.log("Your ans is : ", ans);
+});
