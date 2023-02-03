ffc31aeec9ed8efa4f0f8ae0227c64f441a2d319
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-03 12:23:11 +0530

    practise

--- Node Modules/ask.js
+++ Node Modules/ask.js
@@ -4,6 +4,22 @@ const rl = readline.createInterface({
 	output: process.stdout,
 });
 
-rl.question("How do you like node?", (ans) => {
-	console.log("Your ans is :", ans);
-});
+// rl.question("How do you like node?", (ans) => {
+// 	console.log("Your ans is :", ans);
+// });
+
+const questions = ["What is your name?", "Where do you live?", "What is your age?"];
+
+function collectAnswers(questions, done) {
+	const answers = [];
+
+	function questionAnswered(answer) {
+		answers.push(answer);
+		if (answers.length < questions.length) {
+			rl.question(questions[answers.length], questionAnswered);
+		} else return done(answers);
+	}
+	rl.question(questions[0], questionAnswered);
+}
+
+collectAnswers(questions, (answers) => console.log(answers));
